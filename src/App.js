4b9a7be795aa5d6825435248e7252216b9efb051
PaniNastia <pomazuevanasta@gmail.com>
2024-05-26 17:11:05 +0200

    first day: hero

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import menuImg from "./menu.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id="HomePage">
+        <div>
+          <h1>
+            <a href="/">smart</a>
+          </h1>
+          <a href="ff">
+            <img alt="menu" src={menuImg} />
+          </a>
+        </div>
+        <div>
+          <h2>Welcome to a Smarter Home Experience.</h2>
+          <p>
+            Purus pellentesque sed enim pharetra. Et elit egestas vitae in ut
+            vitae. Cras pulvinar massa ut amet egestas in proin. In hendrerit
+            amet nullam auctor lacinia nisi.
+          </p>
+          <p className="center">
+            <a className="btn" href="#q">
+              Contact us
+            </a>
+          </p>
+        </div>
+        <div>
+          <dl>
+            <dd>50+</dd>
+            <dt>zrealizowanych projektow</dt>
+          </dl>
+          <dl>
+            <dd>10+</dd>
+            <dt>czegos innego</dt>
+          </dl>
+        </div>
+      </section>
+
+      <section id="WhyChooseUs">
+        <div></div>
+      </section>
+
+      <section id="Services">
+        <div></div>
+      </section>
+
+      <section id="Testimonials">
+        <div></div>
+      </section>
+
+      <section id="FAQ">
+        <div></div>
+      </section>
+
+      <footer></footer>
+    </>
   );
 }
 
