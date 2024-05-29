48b65f09ef0b5c7c0e1151eeb6d263af3f52df63
PaniNastia <pomazuevanasta@gmail.com>
2024-05-29 20:50:02 +0200

    Footer, WhChUs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import menuImg from "./menu.png";
+import partner from "./silverPartnerLoxone.png";
 
 function App() {
   return (
@@ -73,23 +74,128 @@ function App() {
                 chroniąc Twój dom i majątek przez całą dobę.
               </p>
             </li>
+            <li>
+              <h4>Innowacyjność i elastyczność</h4>
+              <p>
+                Loxone oferuje zaawansowane technologicznie rozwiązania, które
+                łatwo dostosowują się do indywidualnych potrzeb Twojego domu lub
+                biznesu, pozwalając na tworzenie unikalnych scenariuszy
+                automatyzacji dla maksymalnej efektywności i wygody.
+              </p>
+            </li>
+            <li>
+              <h4>Łatwość instalacji i integracji</h4>
+              <p>
+                System Loxone jest zaprojektowany tak, aby można go było łatwo
+                zainstalować i zintegrować z istniejącymi systemami i
+                urządzeniami w Twoim domu, zapewniając szybkie uruchomienie i
+                bezproblemowe działanie.
+              </p>
+            </li>
           </ol>
         </div>
       </section>
 
       <section id="Services">
-        <div></div>
+        <div>
+          <h2>Our services</h2>
+          <h3>Smart home</h3>
+          <ul>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img alt="" src="" />
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img alt="" src="" />
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img aly="" src="" />
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img alt="" src="" />
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img alt="" src="" />
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+              <img alt="" src="" />
+            </li>
+          </ul>
+        </div>
       </section>
 
       <section id="Testimonials">
-        <div></div>
+        <div>
+          <ul>
+            <li>
+              <h2>Ime Nazwisko</h2>
+              <img alt="" href="" />
+              //stars
+              <p>Ocena</p>
+            </li>
+          </ul>
+        </div>
       </section>
 
       <section id="FAQ">
-        <div></div>
+        <div>
+          <h2>Pytania & odpowiedzi</h2>
+          <h3>Często Zadavane Pytania</h3>
+          <ul>
+            <li>
+              <h4></h4>
+              <p></p>
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+            </li>
+            <li>
+              <h4></h4>
+              <p></p>
+            </li>
+          </ul>
+        </div>
       </section>
 
-      <footer></footer>
+      <footer>
+        <div>
+          <h1>
+            <a href="/">smart</a>
+          </h1>
+        </div>
+
+        <div>
+          <a href="//">
+            <img alt="menu" src={menuImg} />
+          </a>
+          <p>Jakub Wietrzyk</p>
+          <p>
+            Grodzka 14/2
+            <br />
+            58-100 Świdnica
+          </p>
+          <p>NIP PL8852560942</p>
+          <p>+48 729 086 677</p>
+          <p>
+            Obszar działania:
+            <br />
+            Wrocław, Strzegom, Świdnica, <br />
+            Wałbrzych, Oława
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
