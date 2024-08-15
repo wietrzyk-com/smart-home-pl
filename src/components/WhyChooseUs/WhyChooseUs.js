fbbe636e1811c60155c6753484dd933b1fb29415
PaniNastia <pomazuevanasta@gmail.com>
2024-08-15 14:19:16 +0200

    Word fixed

--- src/components/WhyChooseUs/WhyChooseUs.js
+++ src/components/WhyChooseUs/WhyChooseUs.js
@@ -12,49 +12,63 @@ export default function WhyChooseUs() {
           </a> */}
         </p>
         <ol className="custom-list">
-        <li>
+          <li>
             <h4>Sprawdzony producent i świetne wsparcie klienta</h4>
             <p>
-              W swoich rozwiązaniach wykorzystujemy rozwiązania Austriackiej firmy Loxone, która
-              jest obecna na rynku o ponad 15 lat. Firma Loxone ma dwa oddziały w Polsce, 
-              które dbają o to aby Partnerzy Loxone byli dobrze wyszkoleni i znali wszystkie najnowsze rozwiązania.
+              W swoich rozwiązaniach wykorzystujemy rozwiązania Austriackiej
+              firmy Loxone, która jest obecna na rynku o ponad 15 lat. Firma
+              Loxone ma dwa oddziały w Polsce, które dbają o to aby Partnerzy
+              Loxone byli dobrze wyszkoleni i znali wszystkie najnowsze
+              rozwiązania.
             </p>
             <p>
-              Największą siłą rozwiązań Loxone jest oprogramowanie, które pozwala zrealizować nawet
-              najbardziej skomplikowane scenariusze. A my wiemy jak programować, bo mamy ponad 20-letnie doświadczenie w tej branzy.
+              Największą siłą rozwiązań Loxone jest oprogramowanie, które
+              pozwala zrealizować nawet najbardziej skomplikowane scenariusze. A
+              my wiemy jak programować, bo mamy ponad 20-letnie doświadczenie w
+              tej branzy.
             </p>
           </li>
           <li>
             <h4>Doświadczenie i doradztwo</h4>
             <p>
-              Dzięki swojemu doświadczeniu jesteśmy w stanie doradzić najbardzoej 
-              optymalne dopasowane do potrzeb i budzetu klienta. 
+              Dzięki swojemu doświadczeniu jesteśmy w stanie doradzić
+              najbardzoej optymalne dopasowane do potrzeb i budzetu klienta.
             </p>
           </li>
           <li>
             <h4>Oszczędności w zakupach</h4>
             <p>
-              Wybór odpowiedniego sprzętu takiego jak klimatyzator, rekuperator, pompa ciepła jest niezwykle wazny. Nietrafiony zakup potrafi znacznie powiekszyc koszty integracji z systemem automatyki domowej.
+              Wybór odpowiedniego sprzętu takiego jak klimatyzator, rekuperator,
+              pompa ciepła jest niezwykle wazny. Nietrafiony zakup potrafi
+              znacznie powiekszyc koszty integracji z systemem automatyki
+              domowej.
             </p>
-            <p>Dzięki rozwiązaniom smart home często mozna zrezygnować z drogich dodatków, 
-              sterownikow, aplikacji, ktore producenci dołączają do swoich 
-              rozwiązań za dodatkową opłatą. Z maszymi systemami te dodatki 
-              nie są w ogóle potrzebne! Nie potrzebujemy aplikacji do sterowania do rolet, garazu, bramy, drzwi - te rozwiazania zapewnia nasz system.</p>
+            <p>
+              Dzięki rozwiązaniom smart home często mozna zrezygnować z drogich
+              dodatków, sterownikow, aplikacji, ktore producenci dołączają do
+              swoich rozwiązań za dodatkową opłatą. Z maszymi systemami te
+              dodatki nie są w ogóle potrzebne! Nie potrzebujemy aplikacji do
+              sterowania do rolet, garazu, bramy, drzwi - te rozwiazania
+              zapewnia nasz system.
+            </p>
           </li>
           <li>
             <h4>Efektywność energetyczna i oszczędność</h4>
             <p>
-              Inteligentne rozwiązania  pomagają zmniejszyć zużycie
-              energii (do 38%!) optymalizując pracę systemów grzewczych, oświetleniowych
-              i innych urządzeń, co prowadzi do znacznych oszczędności na
-              rachunkach za energię. 
+              Inteligentne rozwiązania pomagają zmniejszyć zużycie energii (do
+              38%!) optymalizując pracę systemów grzewczych, oświetleniowych i
+              innych urządzeń, co prowadzi do znacznych oszczędności na
+              rachunkach za energię.
             </p>
             <p>
-              Nasze systemy integrują się z magazynami energii, fotowoltaiką. Potrafią optymalnie wykorzystać 
-              nazwyzki energii.
+              Nasze systemy integrują się z magazynami energii, fotowoltaiką.
+              Potrafią optymalnie wykorzystać nazwyzki energii.
             </p>
             <p>
-              <strong>Inwestycja w nasze rozwiazania zwraca się szybciej niz fotolotaika :) </strong>
+              <strong>
+                Inwestycja w nasze rozwiazania zwraca się szybciej niz
+                fotowoltaika :){" "}
+              </strong>
             </p>
           </li>
           <li>
